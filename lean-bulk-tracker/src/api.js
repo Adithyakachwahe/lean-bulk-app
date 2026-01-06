f06775b77960459c2bc2c485bf618c6268163353
Kachwahe P Adithya (BD/ERA-AMS2) <dxc1kor@bosch.com>
2026-01-06 16:08:40 +0530

    URL Correction

--- lean-bulk-tracker/src/api.js
+++ lean-bulk-tracker/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // CHANGE THIS TO YOUR ACTUAL BACKEND URL
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://lean-bulk-app.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
